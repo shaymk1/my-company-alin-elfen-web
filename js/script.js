ead28a5e4699ffc7e3104939f4fb6997f31ea99e
unknown <mkekae@gmail.com>
2022-10-08 08:41:15 +0200

    removed extra coding on js file that i did not need and modified the form on index file

--- js/script.js
+++ js/script.js
@@ -9,25 +9,6 @@ document.querySelector("#menu-btn").onclick = () => {
 	loginForm.classList.remove("active");
 };
 
-document.querySelector("#search-btn").onclick = () => {
-	searchForm.classList.toggle("active");
-	navbar.classList.remove("active");
-	loginForm.classList.remove("active");
-};
-
-document.querySelector("#login-btn").onclick = () => {
-	loginForm.classList.toggle("active");
-	navbar.classList.remove("active");
-	searchForm.classList.remove("active");
-};
-
-document.querySelector("#info-btn").onclick = () => {
-	contactInfo.classList.add("active");
-};
-
-document.querySelector("#close-contact-info").onclick = () => {
-	contactInfo.classList.remove("active");
-};
 
 window.onscroll = () => {
 	navbar.classList.remove("active");
@@ -36,65 +17,86 @@ window.onscroll = () => {
 	contactInfo.classList.remove("active");
 };
 
-var swiper = new Swiper(".home-slider", {
-	loop: true,
-	grabCursor: true,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-});
+// var swiper = new Swiper(".home-slider", {
+// 	loop: true,
+// 	grabCursor: true,
+// 	navigation: {
+// 		nextEl: ".swiper-button-next",
+// 		prevEl: ".swiper-button-prev",
+// 	},
+// });
 
-var swiper = new Swiper(".reviews-slider", {
-	loop: true,
-	grabCursor: true,
-	spaceBetween: 20,
-	breakpoints: {
-		640: {
-			slidesPerView: 1,
-		},
-		768: {
-			slidesPerView: 2,
-		},
-		991: {
-			slidesPerView: 3,
-		},
-	},
-});
+// var swiper = new Swiper(".reviews-slider", {
+// 	loop: true,
+// 	grabCursor: true,
+// 	spaceBetween: 20,
+// 	breakpoints: {
+// 		640: {
+// 			slidesPerView: 1,
+// 		},
+// 		768: {
+// 			slidesPerView: 2,
+// 		},
+// 		991: {
+// 			slidesPerView: 3,
+// 		},
+// 	},
+// });
 
-var swiper = new Swiper(".blogs-slider", {
-	loop: true,
-	grabCursor: true,
-	spaceBetween: 20,
-	breakpoints: {
-		640: {
-			slidesPerView: 1,
-		},
-		768: {
-			slidesPerView: 2,
-		},
-		991: {
-			slidesPerView: 3,
-		},
-	},
-});
+// var swiper = new Swiper(".blogs-slider", {
+// 	loop: true,
+// 	grabCursor: true,
+// 	spaceBetween: 20,
+// 	breakpoints: {
+// 		640: {
+// 			slidesPerView: 1,
+// 		},
+// 		768: {
+// 			slidesPerView: 2,
+// 		},
+// 		991: {
+// 			slidesPerView: 3,
+// 		},
+// 	},
+// });
 
-var swiper = new Swiper(".logo-slider", {
-	loop: true,
-	grabCursor: true,
-	spaceBetween: 20,
-	breakpoints: {
-		450: {
-			slidesPerView: 2,
-		},
-		640: {
-			slidesPerView: 3,
-		},
-		768: {
-			slidesPerView: 4,
-		},
-		1000: {
-			slidesPerView: 5,
-		},
-	},
-});
+// var swiper = new Swiper(".logo-slider", {
+// 	loop: true,
+// 	grabCursor: true,
+// 	spaceBetween: 20,
+// 	breakpoints: {
+// 		450: {
+// 			slidesPerView: 2,
+// 		},
+// 		640: {
+// 			slidesPerView: 3,
+// 		},
+// 		768: {
+// 			slidesPerView: 4,
+// 		},
+// 		1000: {
+// 			slidesPerView: 5,
+// 		},
+// 	},
+// });
+
+
+// document.querySelector("#search-btn").onclick = () => {
+// 	searchForm.classList.toggle("active");
+// 	navbar.classList.remove("active");
+// 	loginForm.classList.remove("active");
+// };
+
+// document.querySelector("#login-btn").onclick = () => {
+// 	loginForm.classList.toggle("active");
+// 	navbar.classList.remove("active");
+// 	searchForm.classList.remove("active");
+// };
+
+// document.querySelector("#info-btn").onclick = () => {
+// 	contactInfo.classList.add("active");
+// };
+
+// document.querySelector("#close-contact-info").onclick = () => {
+// 	contactInfo.classList.remove("active");
+// };
