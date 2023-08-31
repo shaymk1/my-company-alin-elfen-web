559446f9e63b26cb51f43935e6d578343e35762c
unknown <mkekae@gmail.com>
2023-08-31 18:05:17 +0200

    finished with the hamburger menu in javascript

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -1,14 +1,4 @@
-// opening the menu
-
-// const navMenu = document.querySelector(".nav__menu");
-// const navBtnOpen = document.querySelector(".nav__toggle-open");
-// const navBtnClose = document.querySelector(".nav__toggle-close");
-
-// const handleMenuOpen = () => {
-// 	navMenu.style.display = "flex";
-// 	navBtnOpen.style.display = "none";
-// 	navBtnClose.style.display = "inline-block";
-// };
+//********** ***// hamburger menu //********** ***//
 
 const navMenu = document.querySelector(".nav__menu");
 const navBtnOpen = document.querySelector(".nav__toggle-open");
@@ -18,3 +8,20 @@ const openNavHandler = () => {
 	navBtnOpen.style.display = "none";
 	navBtnClose.style.display = "inline-block";
 };
+
+const closeNavHandler = () => {
+	navMenu.style.display = "none";
+	navBtnOpen.style.display = "inline-block";
+	navBtnClose.style.display = "none";
+};
+
+navBtnOpen.addEventListener("click", openNavHandler);
+navBtnClose.addEventListener("click", closeNavHandler);
+
+// close the nav menu on click of the nav link on small screens
+const navItems = navMenu.querySelectorAll("a");
+if (window.innerWidth < 768) {
+	navItems.forEach((item) => {
+		item.addEventListener("click", closeNavHandler);
+	});
+}
